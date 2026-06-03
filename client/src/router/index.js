892134f97376ef7e4dcfda7c6f1f071e313bbda5
Huy Vu <vuvanhuy03022002@gmail.com>
2026-06-03 22:03:38 +0700

    fix deploy

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,12 @@
-const Login =
-() => import('../pages/Login.vue')
-
-const Register =
-() => import('../pages/Register.vue')
-const ProductDetail =
-() => import('../pages/ProductDetail.vue')
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Lazy load pages
 const Home = () => import('../pages/Home.vue')
 const Cart = () => import('../pages/Cart.vue')
+const Login = () => import('../pages/Login.vue')
+const Register = () => import('../pages/Register.vue')
+const ProductDetail = () => import('../pages/ProductDetail.vue')
+const Checkout = () => import('../pages/Checkout.vue')
 
 const routes = [
   {
@@ -20,20 +18,20 @@ const routes = [
     component: Cart
   },
   {
-  path:'/product/:id',
-  component:ProductDetail
+    path: '/product/:id',
+    component: ProductDetail
   },
   {
-  path:'/login',
-  component:Login
+    path: '/login',
+    component: Login
   },
   {
-    path:'/register',
-    component:Register
+    path: '/register',
+    component: Register
   },
   {
-  path: '/checkout',
-  component: () => import('../pages/Checkout.vue')
+    path: '/checkout',
+    component: Checkout
   }
 ]
 
